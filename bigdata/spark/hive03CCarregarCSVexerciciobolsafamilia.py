8e0297b7c28d423aba504a12fe268157e95f7fe8
douglasrm87 <61594551+douglasrm87@users.noreply.github.com>
2025-05-26 23:07:06 -0300

    Ajustado select
    Ajustado select

--- bigdata/spark/hive03CCarregarCSVexerciciobolsafamilia.py
+++ bigdata/spark/hive03CCarregarCSVexerciciobolsafamilia.py
@@ -5,14 +5,14 @@ appName= "hive_pyspark"
 master= "local"
 spark = SparkSession.builder.master(master).appName(appName).enableHiveSupport().getOrCreate()
 spark.sql('use faculdade')
-tables = spark.sql("show tables").show()
+#tables = spark.sql("show tables").show()
 
 #spark.sql("load data local inpath './202501_NovoBolsaFamilia.csv'\
 #spark.sql("load data local inpath './amostrabolsafamilia2025JAN.csv'
           
-spark.sql("load data local inpath './202501_NovoBolsaFamilia.csv'\
+spark.sql("load data local inpath './amostrabolsafamilia2025JAN.csv'\
                  overwrite into table bolsafamilia")
 spark.sql("select count (*) from bolsafamilia").show(truncate = False)
 
-spark.sql("select nome_favorecido, count(*) from bolsafamilia group by nome_favorecido having nome_favorecido = 'Tobias'").show(truncate = False)
+spark.sql("select * from bolsafamilia where nome_favorecido = '\"FERNANDA RAMOS TEIXEIRA\"'").show(truncate = False)
 print ("Dados selecionados")
