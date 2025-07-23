442047c6de65224cd4e342c847557fa3a85570ce
douglasrm87 <61594551+douglasrm87@users.noreply.github.com>
2025-07-23 21:49:21 +0000

    introducao ao flask

---
+++ ProjetoDevRapidoPython/restaurante_app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/mesas')
+def mesas():
+    return render_template('mesas.html')
+
+@app.route('/pedido')
+def pedido():
+    return render_template('pedido.html')
+
+@app.route('/pedidos')
+def pedidos():
+    pedidos = [
+        {'id': 101, 'mesa': 'Mesa 1', 'garcom': 'João', 'status': 'Aberto'},
+        {'id': 102, 'mesa': 'Mesa 2', 'garcom': 'Ana', 'status': 'Fechado'}
+    ]
+    return render_template('pedidos.html', pedidos=pedidos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- tkinter/01introtkinter.py
+++ tkinter/01introtkinter.py
@@ -4,9 +4,9 @@ import tkinter as tk
 ### CREATE VIRTUAL DISPLAY ###
 #sudo apt-get install -y xvfb # Install X Virtual Frame Buffer
 #sudo apt install ghostscript python3-tk
-import os
-#os.system('Xvfb :1 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
-os.environ['DISPLAY']=':1'    # tell X clients to use our virtual DISPLAY :1.
+#import os
+#os.system('Xvfb :2 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
+#os.environ['DISPLAY']=':2'    # tell X clients to use our virtual DISPLAY :1.
 
 def exit_button(master):
     master.geometry( "400x400" )
