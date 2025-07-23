030211543fdf2d4f5863fc5bca3494fc89b83f27
douglasrm87 <61594551+douglasrm87@users.noreply.github.com>
2025-07-23 21:23:02 +0000

    ajustes

--- tkinter/01introtkinter.py
+++ tkinter/01introtkinter.py
@@ -1,5 +1,13 @@
 #python -m pip install PySimpleGUI
 import tkinter as tk
+
+### CREATE VIRTUAL DISPLAY ###
+#sudo apt-get install -y xvfb # Install X Virtual Frame Buffer
+#sudo apt install ghostscript python3-tk
+import os
+#os.system('Xvfb :1 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
+os.environ['DISPLAY']=':1'    # tell X clients to use our virtual DISPLAY :1.
+
 def exit_button(master):
     master.geometry( "400x400" )
     button = tk.Button(master,text="Sair",bg="red",fg="white",command=master.quit)
